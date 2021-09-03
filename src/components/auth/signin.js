5f79b54a46212dc6894e8f498cb5718057a27460
AndreAguirreRoman <ankar_aguirre@live.com>
2021-09-02 19:29:34 -0500

    Added the line for the form, and some styles

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 import SignInForm from './signinForm';
+import PageTitle from '../pageTitle';
 
 class SignIn extends Component {
+
+    onSubmit = (fields) => {
+        console.log(fields)
+    }
     render() {
         return (
             <div className='signin'>
-                <SignInForm className='signin__form' />
+                <div className='signin-box'>
+                    <PageTitle className='signin-box__page-title' title='Login' />
+                    <SignInForm onSubmit={this.onSubmit} className='signin-box__form' />
+                </div>
             </div>
         )
     }
--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 
 import { reduxForm, Field } from 'redux-form';
 
-import { FormInput } from '../formFields';
+import { FormInput, FormButton } from '../formFields';
 
 class SignInForm extends Component {
 
     render() {
-        const { className } = this.props;
+        const { className, handleSubmit } = this.props;
         return (
-            <div className={`${className} signin-form`}>
+            <form onSubmit={handleSubmit} className={`${className} signin-form`}>
                 <Field className='signin-form__email' type='email' placeholder='Email' title='Email' name='email' component={FormInput} />
                 <Field className='signin-form__password' type='password' placeholder='Password' title='Password' name='password' component={FormInput} />
-            </div>
+                <div className='signin-form__line'></div>
+                <Field className='signin-form__login' onClick={() => console.log('submits?')} type='submit' title='Login' name='login' component={FormButton} />
+            </form>
         )
     }
 }
