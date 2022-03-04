c61576dd70a12ec348a64f184662e79f69d380ba
AndreAguirreRoman <ankar_aguirre@live.com>
2022-03-04 16:22:33 -0600

    procfile fixeddddss

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.PORT || 8080;
+const port = listen(process.env.PORT);
 const app = express();
 
 app.use(express.static(__dirname + '/dist/'));
